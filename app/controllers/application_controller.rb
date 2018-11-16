36c21ae007a1b5603a0baadaab8c75784a36ab0b
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-11-16 18:33:11 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,6 +22,11 @@ class ApplicationController < Sinatra::Base
     redirect to "/recipes"
   end
   
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    erb :recipe
+  end
+  
   post '/recipes/:id/delete' do
     erb :delete
   end
@@ -34,11 +39,6 @@ class ApplicationController < Sinatra::Base
   patch '/recipes/:id' do
     @recipe = Recipe.update(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
     redirect "/recipes/:id"
-  end
-  
-  get '/recipes/:id' do
-    @recipe = Recipe.find(params[:id])
-    erb :recipe
   end
 
 end
