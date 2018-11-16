534bc8f40e3b234a534cac04943a0350fbff918f
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-11-16 19:07:30 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,6 @@ class ApplicationController < Sinatra::Base
   
   post '/recipes/new' do
     @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
-    binding.pry
     redirect to "/recipes/#{@recipe.id}"
   end
   
